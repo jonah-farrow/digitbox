4cde1d75836aaaf5e6846cb5c49b28a3b79b5585
= <=>
2022-12-14 21:38:17 +0000

    Wee

--- Assets/shootOrbScript.cs
+++ Assets/shootOrbScript.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class shootOrbScript : MonoBehaviour
 {
-    [SerializeField] GameObject teleOrbPrefab;
+    //LineRenderer class to show the arc of the ball
+    [SerializeField] private TrajectoryTracer tracer;
+    //orb prefab to be used to teleport.
+    [SerializeField] TeleOrb teleOrbPrefab;
+    //force that the orb should be thrown at
     [SerializeField] float orbForce = 15f;
+    //Sounds
     [SerializeField] AudioClip shootSound;
     [SerializeField] AudioSource audioSource;
-    List<GameObject> orbsInFlight = new List<GameObject>();
-    GameObject orbInFlight;
+    //Line renderer used to render the aim trajectory of the teleOrbs
+    [SerializeField] private LineRenderer aimLineRenderer;
+    //Number of segments to be used when rendering the trajectory, more segements = higher defintion line
+    private int lineSegments = 25;
+    //List of points to be used by the arc equation and line renderer to create the arc.
+    private List<Vector3> lineSegmentPoints = new List<Vector3>();
+    //Not sure if I'll need this, but this is a list of orbs in flight, incase I need to decide the order that they are executed.
+    List<TeleOrb> orbsInFlight = new List<TeleOrb>();
+    //Current orb in flight
+    TeleOrb orbInFlight;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +34,12 @@ public class shootOrbScript : MonoBehaviour
     void Update()
     {
         
+        //Check to see if the player is holding right click, aka aiming
+        if(Input.GetKey("mouse 1"))
+        {
+            UpdateAimTrace();
+            tracer.SimulateTrajectory(teleOrbPrefab, transform.position);
+        }
     }
 
     public void shootOrb()
@@ -27,6 +47,11 @@ public class shootOrbScript : MonoBehaviour
         audioSource.PlayOneShot(shootSound, 0.5f);
         orbInFlight = Instantiate(teleOrbPrefab, transform.position, Quaternion.identity);
         orbsInFlight.Add(orbInFlight);
-        orbInFlight.GetComponent<Rigidbody>().AddForce(transform.forward * orbForce, ForceMode.Impulse);
+        orbInFlight.GetComponent<TeleOrb>().Init(orbForce, transform.forward);
+    }
+
+    public void UpdateAimTrace()
+    {
+
     }
 }
